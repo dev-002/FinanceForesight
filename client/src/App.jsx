e2c39f7d58d6c4dc6181ffb028477d7047f8cac2
Devansh Gupta <devansh.gupta985@gmail.com>
2023-12-05 09:18:50 +0530

    React Change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+
 function App() {
   return (
     <>
-      <div> Hello Dev</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      {/* <!-- footer section --> */}
+      <section className="footer_section">
+        <div className="container">
+          <p>
+            &copy; <span id="displayYear"></span> All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+          </p>
+        </div>
+        {/* <!-- footer section --> */}
+      </section>
+    </>
+  );
+};
+
+export default Footer;
